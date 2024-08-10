62eab79f7e8a5c302554134fa0b40b8b19f039d0
ELQRONPY <ELQRONPY@gmail.com>
2024-08-10 20:25:27 +0300

    this version 1.2.1

--- lib/models/quran_model.dart
+++ lib/models/quran_model.dart
@@ -6,13 +6,19 @@ class QuranModel {
   final String surahName;
   final Color color;
   final String surahSound;
+
+  // كائن AudioPlayer ثابت
+  static final AudioPlayer _player = AudioPlayer();
+
   const QuranModel({
     required this.surahName,
     required this.color,
     required this.surahSound,
   });
-  void playSound() {
-    final player = AudioPlayer();
-    player.play(AssetSource(surahSound));
+
+  void playSound() async {
+    // إيقاف الصوت الحالي قبل تشغيل السورة الجديدة
+    await _player.stop();
+    await _player.play(AssetSource(surahSound));
   }
 }
